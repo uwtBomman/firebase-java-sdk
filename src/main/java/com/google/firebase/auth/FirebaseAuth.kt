e0270f49470d7c086b33d3e401c4b08dad1437d8
tbomman <tbomman@uwaterloo.ca>
2024-03-28 20:24:34 -0400

    try printing messages

--- src/main/java/com/google/firebase/auth/FirebaseAuth.kt
+++ src/main/java/com/google/firebase/auth/FirebaseAuth.kt
@@ -278,6 +278,8 @@ class FirebaseAuth constructor(val app: FirebaseApp) : InternalAuthProvider {
             @Throws(IOException::class)
             override fun onResponse(call: Call, response: Response) {
                 if (!response.isSuccessful) {
+                    println(response.message())
+                    println(response.body())
                     source.setException(FirebaseAuthInvalidUserException(
                         response.message(),
                         formatErrorMessage("verifyPassword", request, response)
